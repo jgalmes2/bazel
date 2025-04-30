4f28c50f326683fafac2fd71008e68433ffbb010
Jose Galmes <jgalmes@google.com>
2025-04-30 14:43:05 -0700

    More cleanup of CasFileSystem class.

--- src/main/java/com/google/devtools/build/lib/vfs/CasFileSystem.java
+++ src/main/java/com/google/devtools/build/lib/vfs/CasFileSystem.java
@@ -13,28 +13,14 @@
 // limitations under the License.
 package com.google.devtools.build.lib.vfs;
 
-import com.google.devtools.build.lib.clock.Clock;
-import com.google.devtools.build.lib.clock.JavaClock;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.ProfilerTask;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.file.AccessDeniedException;
-import java.nio.file.FileSystemException;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.LinkOption;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
-import java.util.Arrays;
-import java.util.Collection;
-import javax.annotation.Nullable;
 
 /**
  * A FileSystem that uses extended file attributes to obtain a files's digest.
